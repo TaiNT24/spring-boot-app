6761bda1fea2c0b1b017af5e332a6117f3f42c7d
TaiNT24 <nguyentutai12@gmail.com>
2024-11-09 11:49:05 +0700

    change

--- src/main/java/com/example/backend/MyCommandLineRunner.java
+++ src/main/java/com/example/backend/MyCommandLineRunner.java
@@ -14,16 +14,16 @@ public class MyCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Product product = new Product();
-        product.setTitle("iPhone");
-        product.setPrice(1200D);
-        product.setQuantity(99);
-        productService.save(product);
+        // Product product = new Product();
+        // product.setTitle("iPhone");
+        // product.setPrice(1200D);
+        // product.setQuantity(99);
+        // productService.save(product);
 
-        Product product2 = new Product();
-        product2.setTitle("Blackberry");
-        product2.setPrice(1100D);
-        product2.setQuantity(55);
-        productService.save(product2);
+        // Product product2 = new Product();
+        // product2.setTitle("Blackberry");
+        // product2.setPrice(1100D);
+        // product2.setQuantity(55);
+        // productService.save(product2);
     }
 }
